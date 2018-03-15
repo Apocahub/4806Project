c356416552f1f757206e214ca655bb7187395af9
conlanlafreniere <apocamail@gmail.com>
2018-03-15 11:55:56 -0400

    Implemented rudimentary login functionality using sessionId and cookies

--- src/main/java/sysc4806/pm4y/controllers/LoginController.java
+++ src/main/java/sysc4806/pm4y/controllers/LoginController.java
@@ -3,47 +3,67 @@ package sysc4806.pm4y.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 import sysc4806.pm4y.models.Prof;
 import sysc4806.pm4y.models.ProjectCoordinator;
 import sysc4806.pm4y.models.Student;
+import sysc4806.pm4y.models.User;
 import sysc4806.pm4y.repositories.UserRepo;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class LoginController {
     private final UserRepo repo;
+
     @Autowired
     public LoginController(final UserRepo repo) {
         this.repo = repo;
     }
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String login(Model model) {
-        model.addAttribute("prof", new Prof());
-        model.addAttribute("student", new Student());
-        model.addAttribute("coordinator", new ProjectCoordinator());
+    public String login(Model model,
+                        HttpServletResponse response,
+                        @CookieValue(value="sessionId",defaultValue="") String sessionId) {
+
+        if(!sessionId.equals("")) {
+            response.addCookie(new Cookie("sessionId", null));
+        } else {
+            //Log in user, using the existing sessionId
+        }
+
+        model.addAttribute("user", new User());
         return "login";
     }
 
     @RequestMapping(value = "/createProfessor",method = RequestMethod.POST)
-    public String createProfessor(@ModelAttribute(value = "prof") Prof prof)
-    {
-        repo.save(prof);
-        return "profLandingPage";
+    public RedirectView createProfessor(Model model,
+                                        @ModelAttribute("user") User user,
+                                        RedirectAttributes ra) {
+        ra.addFlashAttribute("user", user);
+        ra.addFlashAttribute("type", "prof");
+        return new RedirectView("login");
     }
 
     @RequestMapping(value = "/createStudent",method = RequestMethod.POST)
-    public String createStudent(@ModelAttribute(value = "student") Student student) {
-        repo.save(student);
-        return "studentLandingPage";
+    public RedirectView createStudent(Model model,
+                                      @ModelAttribute("user") User user,
+                                      RedirectAttributes ra) {
+        ra.addFlashAttribute("user", user);
+        ra.addFlashAttribute("type", "stu");
+        return new RedirectView("login");
     }
 
     @RequestMapping(value = "/createCoordinator",method = RequestMethod.POST)
-    public String createProfessor(@ModelAttribute(value = "coordinator") ProjectCoordinator projectCoordinator) {
-        repo.save(projectCoordinator);
-        return "adminLandingPage";
+    public RedirectView createAdmin(Model model,
+                              @ModelAttribute("user") User user,
+                              RedirectAttributes ra) {
+        ra.addFlashAttribute("user", user);
+        ra.addFlashAttribute("type", "admin");
+        return new RedirectView("login");
     }
 }
---
+++ src/main/java/sysc4806/pm4y/controllers/VerificationController.java
@@ -0,0 +1,63 @@
+package sysc4806.pm4y.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import sysc4806.pm4y.models.User;
+import sysc4806.pm4y.repositories.UserRepo;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+@Controller
+public class VerificationController {
+
+    private int sessionCount = 0;
+
+    private UserRepo repo;
+    @Autowired
+    public void instantiateRepo(UserRepo repo) {
+        this.repo = repo;
+    }
+
+    @RequestMapping(value="/login")
+    public String login(@ModelAttribute(value = "user") User user,
+                              @ModelAttribute(value = "type") String type,
+                              HttpServletResponse response) {
+
+        //List<User> accounts = repo.findByEmail(user.getEmail());
+        //Query repo for account and check if exists
+
+        if(user.getPassword().equals("test")) { // true if account exists
+            user.setSessionId("u" + sessionCount++);
+            //repo.save(user);
+            response.addCookie(new Cookie("sessionId", user.getSessionId()));
+            if(type.equals("prof")) {
+                return "profLandingPage";
+            } else if (type.equals("stu")) {
+                return "studentLandingPage";
+            } else if (type.equals("admin")) {
+                return "adminLandingPage";
+            }
+        } else {
+            //password incorrect
+        }
+
+
+        response.addCookie(new Cookie("sessionId", null));
+        return "redirect:/";
+    }
+
+    @RequestMapping(value="/logout", method= RequestMethod.GET)
+    public ModelAndView logout(Model model, HttpServletResponse response) {
+        response.addCookie(new Cookie("sessionId", null));
+        return new ModelAndView("redirect:/");
+    }
+
+}
