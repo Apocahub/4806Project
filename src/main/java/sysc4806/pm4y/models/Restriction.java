67fc8004a5562050f874113e9e013a1a67c051e4
conlanlafreniere <apocamail@gmail.com>
2018-03-29 11:02:48 -0400

    Added Restriction Enum for all engineering streams, with a string return function

---
+++ src/main/java/sysc4806/pm4y/models/Restriction.java
@@ -0,0 +1,53 @@
+package sysc4806.pm4y.models;
+
+import javax.print.attribute.standard.MediaSize;
+
+public enum Restriction {
+    AEROSPACE,
+    ARCHITECTURE,
+    BIOMED_ELEC,
+    BIOMED_MECH,
+    CIVIL,
+    COMMUNICATIONS,
+    COMPUTER_SYSTEMS,
+    ELECTRICAL,
+    ENG_PHYSICS,
+    ENVIRONMENTAL,
+    MECHANICAL,
+    SOFTWARE,
+    SUSTAINABLE_RENEWABLE;
+
+    public String getStreamName(Restriction restriction) {
+        switch (restriction) {
+            case AEROSPACE:
+                return "Aerospace Engineering";
+            case CIVIL:
+                return "Civil Engineering";
+            case SOFTWARE:
+                return "Software Engineering";
+            case ELECTRICAL:
+                return "Electrical Engineering";
+            case MECHANICAL:
+                return "Mechanical Engineering";
+            case BIOMED_ELEC:
+                return "Biomedical & Electrical Engineering";
+            case BIOMED_MECH:
+                return "Biomedical & Mechanical Engineering";
+            case ENG_PHYSICS:
+                return "Engineering Physics";
+            case ARCHITECTURE:
+                return "Architectural Engineering";
+            case ENVIRONMENTAL:
+                return "Environmental Engineering";
+            case COMMUNICATIONS:
+                return "Communications Engineering";
+            case COMPUTER_SYSTEMS:
+                return "Computer Systems Engineering";
+            case SUSTAINABLE_RENEWABLE:
+                return "Sustainable & Renewable Engineering";
+            default:
+                return "ERROR: NOT A VALID ENGINEERING STREAM";
+        }
+    }
+
+}
